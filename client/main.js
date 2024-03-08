5fea809865a7e0386d8c1c622f4664aec42c7d19
Nitin Misra <nitin.misra84@gmail.com>
2024-03-07 20:05:13 -0700

    new feature added

--- client/main.js
+++ client/main.js
@@ -26,6 +26,16 @@ const alertLuckyNumber = (e) => {
     });
 };
 
+// const alertLuckyNumber = (e) => {
+//   e.preventDefault();
+//   const luckyNumber = luckyNumberInput.value;
+//   axios
+//     .post(`http://localhost:4000/api/lucky-number/${luckyNumber}`)
+//     .then((res) => {
+//       alert(res.data);
+//     });
+// };
+
 complimentBtn.addEventListener("click", getCompliment);
 fortuneButton.addEventListener("click", getFortune);
 luckyNumberForm.addEventListener("submit", alertLuckyNumber);
